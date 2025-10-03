d5fe54b2d3c22df4c95b6ac625855244d46e407f
id2836 <tom.dutaud@eleve.ensai.fr>
2025-10-03 10:38:16 +0000

    Test flop

---
+++ src/tests/test_flop.py
@@ -0,0 +1,33 @@
+import pytest
+from src.business_object.flop import Flop
+
+class TestFlop():
+    def test_flop_valide_ok(self):
+        # GIVEN
+        cartes = [
+            Carte("As", "Pique"),
+            Carte("Roi", "Coeur"),
+            Carte("Dame", "Trèfle")
+        ]
+
+        # WHEN
+        flop = Flop(cartes)
+
+        # THEN
+        assert len(flop.cartes) == 3
+        assert flop.cartes == cartes
+    
+    def test_flop_non_valide(self): 
+        # GIVEN
+        cartes = [
+            Carte("As", "Pique"),
+            Carte("Roi", "Coeur")
+        ]
+
+        # WHEN / THEN
+        with pytest.raises(ValueError) as excinfo:
+            Flop(cartes)
+        assert "Le flop doit contenir entre 3 et 5 cartes" in str(excinfo.value)
+        # THEN
+        assert len(flop.cartes) == 3
+        assert flop.cartes == cartes
