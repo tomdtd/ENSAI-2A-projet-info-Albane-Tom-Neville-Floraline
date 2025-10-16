ae1c1f2fbaf7f1361f6d1a5f8baa501461d3a1a4
albane-rouille <albane.rouille@gmail.com>
2025-10-16 17:37:12 +0000

    ajout tests liste cartes

--- src/tests/test_liste_cartes.py
+++ src/tests/test_liste_cartes.py
@@ -11,3 +11,25 @@ class TestListeCartes():
 
         # THEN
         assert cartes.get_cartes() == [pytest.as_coeur]
+    
+    def test_creation_carte_echec_1(self):
+        # GIVEN
+        liste_cartes = (pytest.as_coeur, pytest.deux_pique)
+
+        message_attendu = f"{liste_cartes} doit être une liste."
+
+        # THEN
+        with pytest.raises(ValueError, match=re.escape(expected_message)):
+            # WHEN
+            ListeCartes(liste_cartes)
+
+    def test_creation_carte_echec_2(self):
+        # GIVEN
+        liste_cartes = [pytest.as_coeur, "2 de Pique"]
+
+        message_attendu = f"Tous les elements de {cartes} doivent être des cartes."
+
+        # THEN
+        with pytest.raises(ValueError, match=re.escape(expected_message)):
+            # WHEN
+            ListeCartes(liste_cartes)
