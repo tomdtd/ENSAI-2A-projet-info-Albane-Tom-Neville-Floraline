4332fdb3f27a1dd01ede3d86be13612c1b2a8b98
neville <nevilletchatchounjatcha@gmail.com>
2025-10-23 09:18:42 +0200

    Ajout de la classe JoueurPartie

---
+++ src/business_object/JoueurPartie.py
@@ -0,0 +1,46 @@
+class JoueurPartie:
+    """
+    Classe d'association liant un Joueur à une Partie.
+    Elle contient les informations spécifiques au joueur pour cette partie.
+    Attributs:
+    ----------
+    joueur : Joueur
+        Le joueur associé à cette partie.
+    siege : Siege
+        Le siège occupé par le joueur dans la partie.       
+    main : Main
+        La main de cartes du joueur dans cette partie.
+    solde_partie : Monnaie
+        Le solde d'argent du joueur pour cette partie.             
+    statut : str
+        Le statut actuel du joueur dans la partie (ex: "actif", "couché", "all-in").
+    mise_tour : Monnaie
+        La mise actuelle du joueur pour le tour de mise en cours.
+    
+    Méthodes:
+    ---------
+    miser(montant: int) -> None
+        Permet au joueur de miser un certain montant.       
+    se_coucher() -> None
+        Permet au joueur de se coucher.
+
+
+    """
+    def __init__(self, joueur: Joueur, siege: Siege, solde_partie: int):
+        self.joueur = joueur
+        self.siege = siege
+        self.main = Main()
+        self.solde_partie = Monnaie(solde_partie) 
+        self.statut = "en attente" 
+        self.mise_tour = Monnaie(0) 
+
+    def miser(self, montant: int):
+        """Le joueur mise un certain montant."""
+        self.solde_partie.debiter(montant)
+        self.mise_tour.crediter(montant)
+        print(f"{self.joueur.pseudo} mise {montant}.")
+
+    def se_coucher(self):
+        """Le joueur se couche."""
+        self.statut = "couché"
+        print(f"{self.joueur.pseudo} se couche.")
