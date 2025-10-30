d899e19281367debe12bc160fb4d9d12a6590abf
Albane Rouille <albane.rouille@gmail.com>
2025-10-30 12:47:09 +0100

    ajout tests main joueur complete

--- src/tests/test_main_joueur_complete.py
+++ src/tests/test_main_joueur_complete.py
@@ -28,16 +28,160 @@ class TestMainJoueurComplete():
             # WHEN
             MainJoueurComplete(liste_cartes)
 
+    def test_combinaison_CarteHaute(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.roi_coeur,
+            pytest.dame_pique,
+            pytest.huit_trefle,
+            pytest.six_coeur,
+            pytest.quatre_pique,
+            pytest.trois_trefle,
+            pytest.deux_carreau
+        ]
+        # WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        # THEN
+        assert cartes.combinaison() == Combinaison.CarteHaute
+
+    def test_combinaison_Paire(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.as_coeur,
+            pytest.as_pique,
+            pytest.roi_coeur,
+            pytest.dame_trefle,
+            pytest.neuf_coeur,
+            pytest.trois_trefle,
+            pytest.deux_carreau
+        ]
+        #WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        #THEN
+        assert cartes.combinaison() == Combinaison.Paire
+
+    def test_combinaison_DoublePaire(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.as_coeur,
+            pytest.as_carreau,
+            pytest.roi_coeur,
+            pytest.roi_pique,
+            pytest.valet_trefle,
+            pytest.deux_pique,
+            pytest.trois_trefle
+        ]
+        #WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        #THEN
+        assert cartes.combinaison() == Combinaison.DoublePaire
+
+    def test_combinaison_Brelan(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.dame_coeur,
+            pytest.dame_pique,
+            pytest.dame_trefle,
+            pytest.roi_coeur,
+            pytest.valet_coeur,
+            pytest.deux_pique,
+            pytest.trois_trefle
+        ]
+        #WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        #THEN
+        assert cartes.combinaison() == Combinaison.Brelan
+
+    def test_combinaison_Quinte(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.cinq_coeur,
+            pytest.six_pique,
+            pytest.sept_trefle,
+            pytest.huit_coeur,
+            pytest.neuf_pique,
+            pytest.as_carreau,
+            pytest.deux_trefle
+        ]
+        #WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        #THEN
+        assert cartes.combinaison() == Combinaison.Quinte
+
+    def test_combinaison_Flush(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.as_coeur,
+            pytest.roi_coeur,
+            pytest.valet_coeur,
+            pytest.six_coeur,
+            pytest.deux_coeur,
+            pytest.trois_trefle,
+            pytest.quatre_pique
+        ]
+        #WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        #THEN
+        assert cartes.combinaison() == Combinaison.Flush
+
+    def test_combinaison_Full(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.as_coeur,
+            pytest.as_carreau,
+            pytest.as_pique,
+            pytest.roi_coeur,
+            pytest.roi_pique,
+            pytest.valet_coeur,
+            pytest.dix_coeur
+        ]
+        # WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        # THEN
+        assert cartes.combinaison() == Combinaison.Full
+
+    def test_combinaison_Carre(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.as_coeur,
+            pytest.as_carreau,
+            pytest.as_pique,
+            pytest.as_trefle,
+            pytest.roi_coeur,
+            pytest.deux_pique,
+            pytest.trois_trefle
+        ]
+        #WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        #THEN
+        assert cartes.combinaison() == Combinaison.Carre
+
+    def test_combinaison_QuinteFlush(self):
+        # GIVEN
+        liste_cartes = [
+            pytest.cinq_coeur,
+            pytest.six_coeur,
+            pytest.sept_coeur,
+            pytest.huit_coeur,
+            pytest.neuf_coeur,
+            pytest.as_pique,
+            pytest.deux_trefle
+        ]
+        #WHEN
+        cartes = MainJoueurComplete(liste_cartes)
+        #THEN
+        assert cartes.combinaison() == Combinaison.QuinteFlush
+
     def test_combinaison_QuinteRoyale(self):
         # GIVEN
         liste_cartes = [
-            Carte("As", "Coeur"),
-            Carte("Roi", "Coeur"),
-            Carte("Dame", "Coeur"),
-            Carte("Valet", "Coeur"),
-            Carte("10", "Coeur"),
-            Carte("2", "Pique"),
-            Carte("3", "Trêfle")
+            pytest.as_coeur,
+            pytest.roi_coeur,
+            pytest.dame_coeur,
+            pytest.valet_coeur,
+            pytest.dix_coeur,
+            pytest.deux_pique,
+            pytest.trois_trefle
         ]
 
         # WHEN
