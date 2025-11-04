9a58b83111b345b80e168d6e08ff6914e4da4380
id2836 <tom.dutaud@eleve.ensai.fr>
2025-11-04 09:42:09 +0000

    modification des classes pot et monnaie

--- src/business_object/pot.py
+++ src/business_object/pot.py
@@ -1,7 +1,8 @@
 """Implémentation de la classe Pot."""
 
 from src.business_object.transaction import Transaction
-from datetime import datetime
+from src.business_object.monnaie import Monnaie
+
 
 
 class Pot:
@@ -20,3 +21,17 @@ class Pot:
     __joueurs_contributeurs : list[JoueurPartie]
         Liste des transactions ayant alimenté le pot.
     """
+    def __init__(self):
+        self.montant_pot = Monnaie(0)
+
+    def ajouter_mise(self, montant: int):
+        """Ajoute une mise au pot."""
+        self.montant_pot.crediter(montant)
+
+    def reinitialiser_pot(self):
+        """Remet le pot à zéro."""
+        self.montant_pot = Monnaie(0)
+
+    def get_montant(self) -> int:
+        """Retourne le montant total du pot."""
+        return self.montant_pot.get()
