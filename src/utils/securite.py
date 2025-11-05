bf2875c8e3031861fe7edd7ca302f575a386e77e
id2836 <tom.dutaud@eleve.ensai.fr>
2025-11-05 10:01:03 +0000

    ajout d'une methode dans utils_securite pour verifier le mdp

--- src/utils/securite.py
+++ src/utils/securite.py
@@ -6,3 +6,7 @@ def hash_password(password, sel=""):
     password_bytes = password.encode("utf-8") + sel.encode("utf-8")
     hash_object = hashlib.sha256(password_bytes)
     return hash_object.hexdigest()
+
+def verify_password(password, hashed_password, sel=""):
+    """Vérifie si le mot de passe correspond au hash"""
+    return hash_password(password, sel) == hashed_password
