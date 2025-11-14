55de97e0344019822a182cf4f736f6d1351d2693
albane-rouille <albane.rouille@gmail.com>
2025-11-14 17:09:05 +0000

    rejoindre partie

--- src/view/rejoindre_partie.py
+++ src/view/rejoindre_partie.py
@@ -16,23 +16,35 @@ class RejoindrePartie(VueAbstraite):
 
         print("\n" + "-" * 50 + "\nRejoindre une partie\n" + "-" * 50 + "\n")
 
-        choix_table = inquirer.select(
-            message="Choisissez une table à rejoindre :",
-            choices=[f"Table {t.id_table}" for t in tables_disponibles] + ["Retour au menu"],
+        choix_tables = [f"Table {t.id_table}" for t in tables_disponibles]
+        choix_tables.append("Créer une nouvelle table")
+        choix_tables.append("Retour au menu")
+
+        choix = inquirer.select(
+            message="Que voulez-vous faire ?",
+            choices=choix_tables,
         ).execute()
 
         if choix_table == "Retour au menu":
             from view.menu_joueur_vue import MenuJoueurVue
             return MenuJoueurVue()
         
-        table_choisie = next(t for t in tables_disponibles if f"Table {t.id_table}" == choix_table)
-        success = table_service.rejoindre_table(joueur, table_choisie.id_table)
+        elif choix == "Créer une nouvelle table":
+            # Crée une nouvelle table via le service
+            nouvelle_table = table_service.creer_table(joueur)
+            if nouvelle_table:
+                print(f"Nouvelle table créée ! Table {nouvelle_table.id_table}")
+            else:
+                print("Erreur lors de la création de la table.")
 
-        if success:
-            print(f"Vous avez rejoint la Table {table_choisie.id_table} !")
-        else:
-            print("Impossible de rejoindre cette table, elle est peut-être pleine.") #mettre une condition du nb tables
+        else:        
+            table_choisie = next(t for t in tables_disponibles if f"Table {t.id_table}" == choix_table)
+            success = table_service.rejoindre_table(joueur, table_choisie.id_table)
 
+            if success:
+                print(f"Vous avez rejoint la Table {table_choisie.id_table} !")
+            else:
+                print("Impossible de rejoindre cette table, elle est peut-être pleine.") #mettre une condition du nb tables
 
         from view.menu_joueur_vue import MenuJoueurVue
         return MenuJoueurVue()
